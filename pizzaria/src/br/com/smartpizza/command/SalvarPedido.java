5396641b4bd0bca840febe9ae0a06faa23c86b5d
Jackson <Jackson@DESKTOP-7R0NFNO.lan>
2019-10-10 20:52:27 -0300

    atualizando script banco de dados

--- pizzaria/src/br/com/smartpizza/command/SalvarPedido.java
+++ pizzaria/src/br/com/smartpizza/command/SalvarPedido.java
@@ -1,6 +1,5 @@
 package br.com.smartpizza.command;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,8 +7,7 @@ public class SalvarPedido implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		int idProduto =  Integer.parseInt(request.getParameter("produto"));
-		int idCliente =  Integer.parseInt(request.getParameter("idCLiente"));
+		
 		String proximo = "pedido.jsp";
 		String sabor[] = request.getParameterValues("sabor");
 		String ingredientes[] = request.getParameterValues("ingredientes");
