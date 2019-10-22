18b9eb2250e0984f5eb5c08db34b5d0e43f1c03e
jackson <jackson.novaes@live.com>
2019-10-22 14:12:17 -0300

    alterando test

--- pizzaria/src/br/com/smartpizza/util/Teste.java
+++ pizzaria/src/br/com/smartpizza/util/Teste.java
@@ -7,7 +7,7 @@ import br.com.smartpizza.model.Pessoa;
 
 public class Teste {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		try {
 			Connection con = ConexaoUtil.getConexao();
 			System.out.println("ok");
@@ -15,7 +15,7 @@ public class Teste {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-=======
+
 	try {
 		Connection con = ConexaoUtil.getConexao();
 		System.out.println("ok");
@@ -23,7 +23,7 @@ public class Teste {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
->>>>>>> e2c77ebe267f9309433572e46cd441698ab56ee0
+
 		
 
 	}
